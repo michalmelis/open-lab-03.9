6dcde690bf229e254a61301cc3bc4446641918a7
michalmelis <michalmelis.sk@gmail.com>
2019-11-04 12:31:56 +0100

    Done.

---
+++ open-lab-03.9/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace open_lab_03._9
+{
+    class Program
+    {
+        public static bool IsPrimeNumber(int num)
+        {
+            for (int i = 2; i < num; i++)
+                if (num % i == 0)
+                {
+                    return false;
+                }
+            return true;
+        }
+    }
+}
